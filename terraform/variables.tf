a3b1a3ed8422c912de0fca949b5f3311f821a18a
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-10 09:26:13 -0400

    Create private and public subnets for vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,4 @@
+#VPC Creation
 variable "region" {
   description = "AWS region where resources will be created"
   type        = string
@@ -11,4 +12,14 @@ variable "vpc_cidr" {
 variable "vpc_name" {
   description = "Name tag for the VPC"
   type        = string
+}
+
+#Subnets
+variable "subnet_count" {
+  description = "Number of public and private subnets to create in each availability zone"
+  type        = number
+  validation {
+    condition     = var.subnet_count > 0 && var.subnet_count <= 3
+    error_message = "Subnet count must be between 1 and 3."
+  }
 }
