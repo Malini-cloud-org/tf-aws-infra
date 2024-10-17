a1c552217cd9616f1dd4823a20a889f8a42b201b
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-17 14:17:23 -0400

    Update variable code to terraform template

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,6 +14,22 @@ variable "vpc_name" {
   type        = string
 }
 
+variable "ami_id" {
+  description = "The AMI ID of the custom image."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The EC2 instance type."
+  type        = string
+  default     = "t2.small"
+}
+
+variable "app_port" {
+  description = "The port on which the application will run."
+  type        = number
+  default     = 3000
+}
 #Subnets
 variable "subnet_count" {
   description = "Number of public and private subnets to create in each availability zone"
