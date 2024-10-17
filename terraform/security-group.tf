7c1b22004bb75ab52d1522ad0756a5071b375302
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-17 14:15:11 -0400

    Add security group to terraform template

---
+++ terraform/security-group.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "app_sg" {
+  name        = "application_security_group"
+  description = "Security group for web application"
+
+  vpc_id = aws_vpc.csye6225_vpc.id # Reference the dynamically created VPC
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS access"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Custom app port access"
+    from_port   = var.app_port
+    to_port     = var.app_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
