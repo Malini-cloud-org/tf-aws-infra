1a57d7220ce7d8550b6d86fe8100d9ef36d49637
Malini Janaki Sankaran <janakisankaran.m@northeastern.edu>
2024-10-24 11:52:12 -0400

    Update variables, aws profile default and outputs terraform files

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,8 @@ output "vpc_tags" {
   description = "Tags assigned to the VPC"
   value       = aws_vpc.csye6225_vpc.tags
 }
+
+output "rds_endpoint" {
+  value       = aws_db_instance.rds_instance.endpoint
+  description = "The endpoint of the RDS instance"
+}
